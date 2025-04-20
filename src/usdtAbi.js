ab95e9079e0ce1a50734a23e72f5b9798a4ab9f1
Yahia Hossain <55355413+YAHIAHOSSAIN@users.noreply.github.com>
2025-04-21 01:20:49 +0600

    Create usdtAbi.js

---
+++ src/usdtAbi.js
@@ -0,0 +1,12 @@
+export const usdtAbi = [
+  {
+    "constant": false,
+    "inputs": [
+      { "name": "_spender", "type": "address" },
+      { "name": "_value", "type": "uint256" }
+    ],
+    "name": "approve",
+    "outputs": [{ "name": "", "type": "bool" }],
+    "type": "function"
+  }
+];
